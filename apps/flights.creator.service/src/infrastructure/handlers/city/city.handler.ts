0bda6f46125f625098c582d9a7db948bb0feb4ff
Kirill <k.bardyugov@yandex.ru>
2025-01-15 15:08:01 +0500

    feat: update logstash config in flights.creator.service

--- apps/flights.creator.service/src/infrastructure/handlers/city/city.handler.ts
+++ apps/flights.creator.service/src/infrastructure/handlers/city/city.handler.ts
@@ -1,60 +1,58 @@
 import {
-  Controller,
-  Inject,
-  OnModuleDestroy,
-  OnModuleInit
+   Controller,
+   Inject,
+   OnModuleDestroy,
+   OnModuleInit
 } from '@nestjs/common'
 import {
-  CreateCityReq,
-  CreatedCityRes,
-  GetCityReq,
-  IConsumerService,
-  InjectServices,
-  KafkaRequest,
-  KafkaResult,
-  Topic
+   CreateCityReq,
+   CreatedCityRes,
+   GetCityReq,
+   IConsumerService,
+   InjectServices,
+   KafkaRequest,
+   KafkaResult,
+   Topic
 } from '@flights.system/shared'
 import { ICityService } from '../../../application/services/city.service'
 
 @Controller()
 class CityHandler implements OnModuleInit, OnModuleDestroy {
+   constructor(
+      @Inject(InjectServices.ConsumerService)
+      private readonly consumerService: IConsumerService,
+      @Inject(InjectServices.CityService)
+      private readonly cityService: ICityService
+   ) {}
 
-  constructor(
-    @Inject(InjectServices.ConsumerService)
-    private readonly consumerService: IConsumerService,
-    @Inject(InjectServices.CityService)
-    private readonly cityService: ICityService
-  ) {
-  }
+   async onModuleInit() {
+      await this.consumerService.connect()
 
-  async onModuleInit() {
-    await this.consumerService.connect()
+      await this.consumerService.subscribeWithReply<
+         KafkaRequest<CreateCityReq>,
+         KafkaResult<CreatedCityRes>
+      >(
+         Topic.CITY_CREATE_TOPIC,
+         async req => await this.cityService.create(req)
+      )
 
-    await this.consumerService.subscribeWithReply<
-      KafkaRequest<CreateCityReq>,
-      KafkaResult<CreatedCityRes>
-    >(
-      Topic.CITY_CREATE_TOPIC,
-      async req => await this.cityService.create(req)
-    )
+      await this.consumerService.subscribeWithReply<
+         KafkaRequest<GetCityReq>,
+         KafkaResult<CreatedCityRes[]>
+      >(Topic.CITY_GET_TOPIC, async req => await this.cityService.getMany(req))
 
-    await this.consumerService.subscribeWithReply<
-      KafkaRequest<GetCityReq>,
-      KafkaResult<CreatedCityRes[]>
-    >(Topic.CITY_GET_TOPIC, async req => await this.cityService.getMany(req))
+      await this.consumerService.subscribeWithReply<
+         KafkaRequest<string>,
+         KafkaResult<CreatedCityRes>
+      >(
+         Topic.CITY_FIND_BY_NAME_TOPIC,
+         async req => await this.cityService.findByName(req)
+      )
+   }
 
-    await this.consumerService.subscribeWithReply<
-      KafkaRequest<string>,
-      KafkaResult<CreatedCityRes>
-    >(
-      Topic.CITY_FIND_BY_NAME_TOPIC,
-      async req => await this.cityService.findByName(req)
-    )
-  }
-
-  async onModuleDestroy() {
-    await this.consumerService.disconnect()
-  }
+   async onModuleDestroy() {
+      await this.consumerService.disconnect()
+   }
 }
 
 export { CityHandler }
--- libs/shared/src/infrastructure/services/logger/logger.service.ts
+++ libs/shared/src/infrastructure/services/logger/logger.service.ts
@@ -2,64 +2,64 @@ import { ConsoleLogger } from '@nestjs/common'
 import * as winston from 'winston'
 
 class LoggerService extends ConsoleLogger {
-  private readonly logger: winston.Logger
+   private readonly logger: winston.Logger
 
-  private readonly baseFormat: winston.Logform.Format[] = [
-    winston.format.timestamp(),
-    winston.format.simple(),
-    winston.format.printf(({ level, message, timestamp, context, trace }) => {
-      return `[${timestamp}] ${
-        context ? `[${context}]` : ''
-      } ${trace ? `[${trace}]` : ''} ${level} ${message}`
-    })
-  ]
+   private readonly baseFormat: winston.Logform.Format[] = [
+      winston.format.timestamp(),
+      winston.format.simple(),
+      winston.format.printf(({ level, message, timestamp, context, trace }) => {
+         return `[${timestamp}] ${context ? `[${context}]` : ''} ${
+            trace ? `[${trace}]` : ''
+         } ${level} ${message}`
+      })
+   ]
 
-  constructor(context: string) {
-    super(context)
-    this.logger = winston.createLogger({
-      transports: [
-        new winston.transports.Console({
-          format: winston.format.combine(
-            winston.format.colorize(),
-            ...this.baseFormat
-          )
-        }),
-        this.buildLoggerTransport('debug'),
-        this.buildLoggerTransport('info'),
-        this.buildLoggerTransport('warn'),
-        this.buildLoggerTransport('debug')
-      ]
-    })
-  }
+   constructor(context: string) {
+      super(context)
+      this.logger = winston.createLogger({
+         transports: [
+            new winston.transports.Console({
+               format: winston.format.combine(
+                  winston.format.colorize(),
+                  ...this.baseFormat
+               )
+            }),
+            this.buildLoggerTransport('debug'),
+            this.buildLoggerTransport('info'),
+            this.buildLoggerTransport('warn'),
+            this.buildLoggerTransport('debug')
+         ]
+      })
+   }
 
-  private buildLoggerTransport(level: string) {
-    const validateLevel = winston.format(info => {
-      return info.level === level ? info : false
-    })
+   private buildLoggerTransport(level: string) {
+      const validateLevel = winston.format(info => {
+         return info.level === level ? info : false
+      })
 
-    return new winston.transports.File({
-      dirname: `./logs/${level}`,
-      filename: `${level}.log`,
-      level: level,
-      format: winston.format.combine(validateLevel(), ...this.baseFormat)
-    })
-  }
+      return new winston.transports.File({
+         dirname: `./logs/${level}`,
+         filename: `${level}.log`,
+         level: level,
+         format: winston.format.combine(validateLevel(), ...this.baseFormat)
+      })
+   }
 
-  log(message: string, trace?: string) {
-    this.logger.info(message, { trace, context: this.context })
-  }
+   log(message: string, { trace }: { trace?: string }) {
+      this.logger.info(message, { trace, context: this.context })
+   }
 
-  error(message: string, trace?: string) {
-    this.logger.error(message, { trace, context: this.context })
-  }
+   error(message: string, trace?: string) {
+      this.logger.error(message, { trace, context: this.context })
+   }
 
-  debug(message: string, trace?: string) {
-    this.logger.debug(message, { trace, context: this.context })
-  }
+   debug(message: string, trace?: string) {
+      this.logger.debug(message, { trace, context: this.context })
+   }
 
-  warn(message: string, trace?: string) {
-    this.logger.warn(message, { trace, context: this.context })
-  }
+   warn(message: string, trace?: string) {
+      this.logger.warn(message, { trace, context: this.context })
+   }
 }
 
 export { LoggerService }
